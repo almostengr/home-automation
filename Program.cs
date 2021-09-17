a922a099e05629aefaabe8ceea0704bfc42e42d0
Almost Engineer <bitsecondal@gmail.com>
2021-09-16 19:21:45 -0500

    Updated README; added project files

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Almostengr.RhtServices.TranscriptCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TRANSCRIPT_FILE_PATH = args[0];
+
+            try
+            {
+                if (TRANSCRIPT_FILE_PATH == string.Empty || TRANSCRIPT_FILE_PATH == null)
+                {
+                    throw new ArgumentException("Transcript file path not provided");
+                }
+
+                string line = string.Empty;
+                List<string> lines = new List<string>();
+
+                // read the file
+
+                StreamReader file = new System.IO.StreamReader(TRANSCRIPT_FILE_PATH);
+                while ((line = file.ReadLine()) != null)
+                {
+                    line = line
+                        .ToUpper()
+                        .Replace("UM", string.Empty)
+                        .Replace("UH", string.Empty)
+                        .Replace("  ", " ")
+                        ;
+
+                    lines.Add(line);
+                }
+
+                file.Close();
+            
+                TRANSCRIPT_FILE_PATH = TRANSCRIPT_FILE_PATH.Replace(".sbv", string.Empty);
+
+                // write transcript
+
+                string OUTPUT_FILE_PATH = TRANSCRIPT_FILE_PATH + ".youtube.sbv";
+                StreamWriter outputFile = new System.IO.StreamWriter(OUTPUT_FILE_PATH);
+                foreach (string lineItem in lines)
+                {
+                    outputFile.WriteLine(lineItem);
+                }
+                outputFile.Close();
+
+                Console.WriteLine("Transcript written to: " + OUTPUT_FILE_PATH);
+
+                // write blog post
+                
+                OUTPUT_FILE_PATH = TRANSCRIPT_FILE_PATH + ".md";
+                outputFile = new System.IO.StreamWriter(OUTPUT_FILE_PATH);
+                foreach (string lineItem in lines)
+                {
+                    if (lineItem.StartsWith("0:") || lineItem == string.Empty)
+                    {
+                        continue;
+                    }
+
+                    outputFile.WriteLine(lineItem);
+                }
+                outputFile.Close();
+
+                Console.WriteLine("Blog post written to: " + OUTPUT_FILE_PATH);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex);
+            }
+        }
+
+        static void DisplayHelp()
+        {
+            Console.WriteLine("Application Usage");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Usage: transcriptcleaner <transcript file>");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Example: transcriptcleaner C:\\Users\\almostengr\\Desktop\\transcript.sbv");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# transcriptcleaner
-Cleans transcripts from YouTube
+# Transcript Cleaner
+
+Cleans transcripts for videos that have been downloaded from YouTube. 
+
+This application is used to create blog posts on by business website [rhtservices.net](https://rhtservices.net) 
+as well as to be used on YouTube after they have been proofread and properly formatted.
+
+To use this application, you will have to compile it like any other .NET Core application either via the 
+command line or using Visual Studio.
+
+The application will output two files.
+
+* First file can be uploaded back to YouTube as a transcript or captions that has some formatting applied to it
+* Second file has the full transcript with the timestamps removed so that it can be used in a blog post or other media
+
+## Example Usage on Linux
+
+```sh
+transcriptcleaner C:\\Users\\almostengr\\Desktop\\transcript.sbv
+```
