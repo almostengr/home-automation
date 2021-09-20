08470c71388381d19da5fc54b9ef1c356e55eaf1
Almost Engineer <bitsecondal@gmail.com>
2021-09-19 22:01:46 -0500

    Closed #4 remove duplicate words and other cleanup methods added

---
+++ Almostengr.TranscriptCleaner.Api/Common/Constants.cs
@@ -0,0 +1,8 @@
+namespace Almostengr.TranscriptCleaner.Api.Common
+{
+    public static class Constants
+    {
+        public static string NewLine = "\n";
+        public static string Space = " ";
+    }
+}
--- Almostengr.TranscriptCleaner.Api/Controllers/v1/TranscriptController.cs
+++ Almostengr.TranscriptCleaner.Api/Controllers/v1/TranscriptController.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using Almostengr.TranscriptCleaner.Api.Common;
 using Almostengr.TranscriptCleaner.Api.DataTransferObjects;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -16,21 +16,24 @@ namespace Almostengr.TranscriptCleaner.Api.Controllers
             _logger = logger;
         }
 
-        [HttpPost]
-        public ActionResult<TranscriptOutputDto> Post(TranscriptInputDto inputDto)
+        [HttpPost("clean")]
+        public ActionResult<TranscriptOutputDto> CleanTranscript(TranscriptInputDto inputDto)
         {
             if (string.IsNullOrEmpty(inputDto.Input))
+            {
+                _logger.LogError("Input is empty");
                 return BadRequest("Input is required");
+            }
 
             string[] inputLines = inputDto.Input.Split('\n');
 
-            if (inputLines[0].StartsWith("1") == false && inputLines[1].StartsWith("0:") == false)
+            if (inputLines[0].StartsWith("1") == false || inputLines[1].StartsWith("00:") == false)
+            {
+                _logger.LogError("Input is not in the correct format");
                 return BadRequest("Input is not in the correct format");
+            }
 
-            List<string> blogOutput = new List<string>();
-            List<string> videoOutput = new List<string>();
             int counter = 0;
-
             string videoString = string.Empty;
             string blogString = string.Empty;
 
@@ -39,22 +42,52 @@ namespace Almostengr.TranscriptCleaner.Api.Controllers
                 counter = counter >= 4 ? 1 : counter + 1;
 
                 string cleanedLine = line.ToUpper()
-                        .Replace("UM", string.Empty)
-                        .Replace("UH", string.Empty)
-                        .Replace("  ", " ");
+                    .Replace("UM", string.Empty)
+                    .Replace("UH", string.Empty)
+                    .Replace("[MUSIC] YOU", "[MUSIC]")
+                    .Replace("  ", " ")
+                    .Trim();
 
-                videoString += cleanedLine + "\n";
+                videoString += cleanedLine + Constants.NewLine;
 
                 if (counter == 3)
-                    blogString += cleanedLine + "\n";
+                    blogString += cleanedLine + Constants.Space;
             }
+
+            blogString = RemoveDupesFromBlogString(blogString);
+            blogString = CleanBlogString(blogString);
 
             TranscriptOutputDto outputDto = new TranscriptOutputDto();
             outputDto.VideoText = videoString;
             outputDto.BlogText = blogString;
+            outputDto.BlogWords = blogString.Split(' ').Length;
 
+            _logger.LogInformation("Transcript processed successfully");
             return Ok(outputDto);
         }
 
+        private string CleanBlogString(string blogText)
+        {
+            return blogText.Replace("[MUSIC]", string.Empty)
+                .Replace("  ", " ")
+                .Trim();
+        }
+
+        private string RemoveDupesFromBlogString(string blogText)
+        {
+            string[] words = blogText.Split(' ');
+            string previousWord = string.Empty;
+            string output = string.Empty;
+
+            foreach (var word in words)
+            {
+                if (previousWord != word)
+                {
+                    output += word + Constants.Space;
+                }
+            }
+
+            return output;
+        }
     }
 }
